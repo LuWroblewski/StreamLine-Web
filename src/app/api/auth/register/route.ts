c8b34cf8d0ad5857d8b9ada16747165ff10719e1
LuWroblewski <wrolucas@gmail.com>
2023-07-01 00:32:37 -0300

    resolve bug backend

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,6 +1,6 @@
 import { Client } from 'pg';
 import * as dotenv from 'dotenv';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import bcrypt from 'bcrypt';
 
 dotenv.config();
@@ -15,9 +15,8 @@ const client = new Client({
   ssl: true,
 });
 
-export async function POST(req: NextRequest) {
-  const data = await req.json();
-
+export async function POST(request: Request) {
+  const data = await request.json();
   await client.connect();
 
   try {
@@ -29,13 +28,9 @@ export async function POST(req: NextRequest) {
     };
     await client.query(query);
     console.log('Dados inseridos com sucesso!');
-    return new NextResponse('Dados inseridos com sucesso!', {
-      status: 200,
-    });
+    return NextResponse.json({ message: 'Dados inseridos com sucesso!', status: 200 });
   } catch (error) {
     console.error('Ocorreu um erro ao inserir os dados: ', error);
-    return new NextResponse('Ocorreu um erro ao inserir os dados.', {
-      status: 500,
-    });
+    return NextResponse.json({ message: 'Ocorreu um erro ao inserir os dados.', status: 500 });
   }
 }
