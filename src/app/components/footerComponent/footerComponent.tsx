ad14e2de25b3376161ae395a802fa1e71402b83e
LuWroblewski <wrolucas@gmail.com>
2023-07-10 13:44:03 -0300

    footer style fixed

--- src/app/components/footerComponent/footerComponent.tsx
+++ src/app/components/footerComponent/footerComponent.tsx
@@ -5,19 +5,19 @@ export const FooterComponent = () => {
 
   return (
     <footer className='bg-gray-800 text-white py-4 w-full h-full min-h-[20vh]  relative top-[29vh] lg:top-[0vh] font-titillium'>
-      <p className='text-center text-xl font-bold tracking-wider relative top-4	'>Seu Nome Aqui</p>
-      <p className='text-center text-base font-bold relative top-7'>
+      <p className='bg-gray-800 text-center text-xl font-bold tracking-wider relative top-4	'>Seu Nome Aqui</p>
+      <p className='bg-gray-800 text-center text-base font-bold relative top-7'>
         <a href={`mailto:${email}`} className='underline'>
           {email}
         </a>
       </p>
-      <p className='text-center text-xs font-bold relative top-14'>
+      <p className='bg-gray-800 text-center text-xs font-bold relative top-14'>
         Feito com ❤️ por:
         <a href='https://github.com/LuWroblewski' target='_blank'>
           LuWroblewski
         </a>
       </p>
-      <p className='text-left text-xs font-bold relative top-6 pl-2 lg:pl-4'>
+      <p className='bg-gray-800 text-left text-xs font-bold relative top-6 pl-2 lg:pl-4'>
         <Link href='/login'>Login </Link>
       </p>
     </footer>
